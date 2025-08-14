5fa2bfd09d73a6036c43b8fdd531ee2b1dd9f422
akbar dwi mulya <pikselmos@gmail.com>
2025-08-15 06:01:56 +0700

    Seaseon 13

--- piksel-mos-admin/lib/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Ganti dengan URL dan Anon Key proyek Supabase Anda
-const supabaseUrl = 'https://yltxsucpzthnzchziakc.supabase.co';
-const supabaseAnonKey = 'YOUR_ANON_KEY'; // Ganti dengan Anon Key Anda
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
